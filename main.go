018fac00526838cab76580e81435891e8596d0af
kelvyn2012 <kudufo@gmail.com>
2025-09-27 09:05:34 +0100

    makinf use of formatted text

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("welcome to conference booking application")
+	var conferenceName = "Go-conference"
+	const ticketNumber = 50
+	var availableTicket = 50
+
+	fmt.Printf("welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v Tickets to be sold, and we have %v Tickets available!!\n", ticketNumber, availableTicket)
 	println("Get your tickets here to attend")
 
 }
