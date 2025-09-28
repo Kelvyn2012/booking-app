3606d2b9a8d7b980b190fbcfc7ede1819fed37a8
kelvyn2012 <kudufo@gmail.com>
2025-09-28 18:48:43 +0100

    working with strings,int and pointers

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go-conference"
-	const ticketNumber = 50
-	var availableTicket = 50
+	// var conferenceName = "Go-conference"
+	// var availableTicket = 50
 
-	fmt.Printf("welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v Tickets to be sold, and we have %v Tickets available!!\n", ticketNumber, availableTicket)
-	println("Get your tickets here to attend")
+	// fmt.Printf("welcome to %v booking application\n", conferenceName)
+	// fmt.Printf("We have total of %v Tickets to be sold, and we have %v Tickets available!!\n", ticketNumber, availableTicket)
+	// println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var ticketNumber int
+	var email string
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your Email")
+	fmt.Scan(&email)
+	fmt.Println("Number of Tickets")
+	fmt.Scan(&ticketNumber)
+
+	fmt.Printf("Thank you %v %v for purchasing %v ticket,your confirmation mail will be sent to the email address you provided %v.\n", firstName, lastName, ticketNumber, email)
 
 }
