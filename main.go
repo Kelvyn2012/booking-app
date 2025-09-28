1662863d2521eab0a57c50046f96bd8c82ff19f4
kelvyn2012 <kudufo@gmail.com>
2025-09-28 19:22:34 +0100

    working with list

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	var conferenceName = "Go-conference"
 	var availableTicket = 50
+	var bookings [50]string
 
 	// fmt.Printf("welcome to %v booking application\n", conferenceName)
 	// fmt.Printf("We have total of %v Tickets to be sold, and we have %v Tickets available!!\n", ticketNumber, availableTicket)
@@ -24,8 +25,12 @@ func main() {
 	fmt.Println("Number of Tickets")
 	fmt.Scan(&ticketNumber)
 	availableTicket = availableTicket - ticketNumber
+	bookings[0] = firstName + " " + lastName
 
 	fmt.Printf("Thank you %v %v for purchasing %v ticket,your confirmation mail will be sent to the email address you provided %v.\n", firstName, lastName, ticketNumber, email)
 	fmt.Printf("%v remaining tickets for the %v.\n", availableTicket, conferenceName)
+	fmt.Println(bookings)
+	fmt.Println(len(bookings))
+	fmt.Println(bookings[0])
 
 }
